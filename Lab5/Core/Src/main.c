85cf0c4cb0a68c9d582727b650b54f1d453dc3d2
Matthew Morales <mtthwmrls9@gmail.com>
2024-03-18 19:11:09 -0600

    Receiving NACKF

--- Lab5/Core/Src/main.c
+++ Lab5/Core/Src/main.c
@@ -60,6 +60,10 @@ void config_red () {
 	GPIOC->OTYPER &= ~(GPIO_OTYPER_OT_6);
 	GPIOC->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEEDR6_Msk);
 	GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR6_Msk);
+	GPIOC->ODR &= ~GPIO_ODR_6;
+}
+void toggle_red () {
+	GPIOC->ODR ^= GPIO_ODR_6;
 }
 void config_blue () {
 	GPIOC->MODER &= ~(GPIO_MODER_MODER7_Msk);
@@ -67,6 +71,10 @@ void config_blue () {
 	GPIOC->OTYPER &= ~(GPIO_OTYPER_OT_7);
 	GPIOC->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEEDR7_Msk);
 	GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR7_Msk);
+	GPIOC->ODR &= ~GPIO_ODR_7;
+}
+void toggle_blue () {
+	GPIOC->ODR ^= GPIO_ODR_7;
 }
 void config_orange () {
 	GPIOC->MODER &= ~(GPIO_MODER_MODER8_Msk);
@@ -74,6 +82,10 @@ void config_orange () {
 	GPIOC->OTYPER &= ~(GPIO_OTYPER_OT_8);
 	GPIOC->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEEDR8_Msk);
 	GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR8_Msk);
+	GPIOC->ODR &= ~GPIO_ODR_8;
+}
+void toggle_orange () {
+	GPIOC->ODR ^= GPIO_ODR_8;
 }
 void config_green () {
 	GPIOC->MODER &= ~(GPIO_MODER_MODER9_Msk);
@@ -81,6 +93,21 @@ void config_green () {
 	GPIOC->OTYPER &= ~(GPIO_OTYPER_OT_9);
 	GPIOC->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEEDR9_Msk);
 	GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR9_Msk);
+	GPIOC->ODR &= ~GPIO_ODR_9;
+}
+void toggle_green () {
+	GPIOC->ODR ^= GPIO_ODR_9;
+}
+
+void i2c_write (char addr, char chip_id_addr, int num_bytes, char* data) {
+	// Prepare a write operation
+	I2C2->CR2 |= (addr << I2C_CR2_SADD_Pos); // Select follower address
+	I2C2->TXDR = chip_id_addr;
+	I2C2->CR2 |= (num_bytes << I2C_CR2_NBYTES_Pos); // Set the number of bytes
+	I2C2->CR2 &= ~I2C_CR2_RD_WRN; // Set to write mode
+	I2C2->CR2 |= I2C_CR2_START; // Start the transaction
+	
+	
 }
 
 /* USER CODE END 0 */
@@ -123,10 +150,16 @@ int main(void)
 	GPIOB->AFR[1] &= ~GPIO_AFRH_AFSEL13_Msk;
 	GPIOB->AFR[1] |= (5 << GPIO_AFRH_AFSEL13_Pos); // SCL
 	
+	// Configure Pullup Resistors for SDA and SCL
+	GPIOB->PUPDR &= ~(GPIO_PUPDR_PUPDR11_Msk);
+	GPIOB->PUPDR &= ~(GPIO_PUPDR_PUPDR13_Msk);
+	GPIOB->PUPDR |= GPIO_PUPDR_PUPDR11_0;
+	GPIOB->PUPDR |= GPIO_PUPDR_PUPDR13_0;
+	
 	// Set GPIO PB14 and PC0 to output mode
 	GPIOB->MODER &= ~(GPIO_MODER_MODER14_Msk);
-	GPIOB->MODER &= ~(GPIO_MODER_MODER14_Msk);
-	GPIOC->MODER |= (1 << GPIO_MODER_MODER0_Pos);
+	GPIOC->MODER &= ~(GPIO_MODER_MODER0_Msk);
+	GPIOB->MODER |= (1 << GPIO_MODER_MODER14_Pos);
 	GPIOC->MODER |= (1 << GPIO_MODER_MODER0_Pos);
 	
 	// Set pins PB14 and PC0 to push-pull
@@ -138,13 +171,19 @@ int main(void)
 	GPIOC->ODR |= GPIO_ODR_0;
 	
 	// Set the I2C2 clock speed to 100kHz
-	I2C2->TIMINGR |= I2C_TIMINGR_PRESC;
+	I2C2->TIMINGR |= (0x1 << I2C_TIMINGR_PRESC_Pos);
 	I2C2->TIMINGR |= (0x13 << I2C_TIMINGR_SCLL_Pos);
 	I2C2->TIMINGR |= (0xF << I2C_TIMINGR_SCLH_Pos);
-	I2C2->TIMINGR |= (0x2 << I2C_TIMINGR_SCLDEL_Pos);
+	I2C2->TIMINGR |= (0x2 << I2C_TIMINGR_SDADEL_Pos);
+	I2C2->TIMINGR |= (0x4 << I2C_TIMINGR_SCLDEL_Pos);
 
 	// Enable the I2C2 Peripheral
 	I2C2->CR1 |= I2C_CR1_PE;
+	
+	config_blue();
+	config_orange();
+	config_red();
+	config_green();
 
   /* USER CODE END Init */
 
@@ -162,9 +201,30 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+	
+	// Prepare a write operation
+	I2C2->CR2 |= (0x69 << I2C_CR2_SADD_Pos); // Select follower address
+	I2C2->CR2 |= (1 << I2C_CR2_NBYTES_Pos); // Set the number of bytes
+	I2C2->CR2 &= ~I2C_CR2_RD_WRN; // Set to write mode
+	I2C2->CR2 |= I2C_CR2_START; // Start the transaction
+	
+	// Wait until we get either a NACK or a confirmation
+	while (!(I2C2->ISR & I2C_ISR_NACKF_Msk) && !(I2C2->ISR & I2C_ISR_TXIS)) {
+		HAL_Delay(1);
+	};
+		
+	if (I2C2->ISR & I2C_ISR_NACKF_Msk) {
+		toggle_red();
+	} else {
+		toggle_green();
+	}
+	
   while (1)
   {
     /* USER CODE END WHILE */
+		
+		toggle_orange();
+		HAL_Delay(1000);
 
     /* USER CODE BEGIN 3 */
   }
